f38e906acb3e5b6176d35e0e3db13822fbb981dc
Khurelbaatar Zolzaya <zoloo.genius@gmail.com>
2025-12-09 23:05:08 +0900

    Категорийн номыг авах болон бүх номыг авах апи-уудыг салгах
    Good design for API one endpoint must be do only one thing

--- controller/books.js
+++ controller/books.js
@@ -6,23 +6,24 @@ const qs = require("qs");
 const path = require("path");
 
 // api/v1/books
-// api/v1/categories/:catId/books
 exports.getBooks = asyncHandler(async (req, res, next) => {
-  let query;
+  console.log("All books");
 
-  if (req.params.categoryId) {
-    console.log("Category query catId: " + req.params.categoryId);
-    query = Book.find({ category: req.params.categoryId });
-  } else {
-    console.log("All books");
-    // If we want to get whole info of category call .populate('category')
-    query = Book.find().populate({
-      path: "category",
-      select: "name averagePrice",
-    });
-  }
+  const books = await Book.find().populate({
+    path: "category",
+    select: "name averagePrice",
+  });
 
-  const books = await query;
+  res.status(200).json({
+    success: true,
+    count: books.length,
+    data: books,
+  });
+});
+
+// api/v1/categories/:catId/books
+exports.getCategoryBooks = asyncHandler(async (req, res, next) => {
+  const books = await Book.find({ category: req.params.categoryId });;
 
   res.status(200).json({
     success: true,
@@ -119,11 +120,10 @@ exports.uploadBookPhoto = asyncHandler(async (req, res, next) => {
 
     book.photo = file.name;
     book.save();
-    
+
     res.status(200).json({
       sucess: true,
       data: file.name,
     });
   });
-  
 });
--- routes/categories.js
+++ routes/categories.js
@@ -9,27 +9,13 @@ const {
   deleteCategory,
 } = require("../controller/categories");
 
+const { getCategoryBooks } = require("../controller/books");
+router.route("/:categoryId/books").get(getCategoryBooks);
 
-/* 1. First method to connect book route in caterogy
- * Import books controller then connect getBooks function
-*/
-//const {getBooks} = require("../controller/books");
-//router.route("/:categoryId/books").get(getBooks);
-
-/*
- 2. Second method import bookRouter 
- Ном роутер оруулж ирнэ тэгээд ямар хаягнаас орж ирхэд Ном унших ёстойг зааж өгнө.
- Зааж өгч байгаа роутер нь заавал {mergeParams : true} болгосон байх ёстойг анхаарах
-
-*/
-const booksRouter = require("./books");
-router.use("/:categoryId/books", booksRouter);
 // api/v1/caterories
 router.route("/").get(getCategories).post(createCategory);
 
-
-
-// 2. Second method: 
+// 2. Second method:
 router
   .route("/:id")
   .get(getCategory)
