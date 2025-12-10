a8fa6166dddee9f451c09d610ad732c8634f4c31
Khurelbaatar Zolzaya <zoloo.genius@gmail.com>
2025-12-10 23:01:59 +0900

    Create JWT token using jwtwebtoken middleware Хэрэглэгчийг амжилтттай бүртгэсний дараа JWT токенийг буцаах
    User Model created getJWT() method function
    
     method function will called from instance of object, static function is called directly from object

--- controller/users.js
+++ controller/users.js
@@ -7,9 +7,11 @@ exports.register = asyncHandler(async (req, res, next) => {
   
   const user = await User.create(req.body);
 
-
+  const jwt = user.getJWT();
   res.status(200).json({
     success: true,
-    data: req.body
+    jwt: jwt,
+    data: user,
+
   });
 });
--- models/User.js
+++ models/User.js
@@ -1,43 +1,52 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Хэрэглэгчийн нэрийг оруулна уу']
-    },
-    email: {
-        type: String,
-        required: [true, 'Хэрэглэгчийн имэйл хаягийг оруулж өгнө үү'],
-        unique: true,
-        match : [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Имэйл хаяг буруу байна.",
-        ],
-    },
-    role: {
-        type: String,
-        required : [true, "Хэрэглэгчийн эрхийг оруулна уу"],
-        enum: ["user", "operator"],
-        default: "user",
-    },
-    password: {
-        type: String,
-        minlength: 4,
-        required: [true, "Нууц үгээ оруулна уу"],
-        select: false,
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date,
-    createAt: {
-        type: Date,
-        default: Date.now,
-    }
+  name: {
+    type: String,
+    required: [true, "Хэрэглэгчийн нэрийг оруулна уу"],
+  },
+  email: {
+    type: String,
+    required: [true, "Хэрэглэгчийн имэйл хаягийг оруулж өгнө үү"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Имэйл хаяг буруу байна.",
+    ],
+  },
+  role: {
+    type: String,
+    required: [true, "Хэрэглэгчийн эрхийг оруулна уу"],
+    enum: ["user", "operator"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    minlength: 4,
+    required: [true, "Нууц үгээ оруулна уу"],
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-UserSchema.pre("save", async function(){
-    const salt = await bcrypt.genSaltSync(10);
-    this.password = await bcrypt.hash(this.password, salt);
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.getJWT = function () {
+  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIREIN,
+  });
+
+  return token;
+};
 
 module.exports = mongoose.model("User", UserSchema);
