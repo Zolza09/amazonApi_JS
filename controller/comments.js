ff02b23848e0a682f81afd9e2e261b77853350a0
Khurelbaatar Zolzaya <zoloo.genius@gmail.com>
2026-02-03 07:32:31 +0900

    Added getComments, deleteComments, updateComments endpoints
     * Raise except for one user multiple comments we need to check database indices.

--- controller/comments.js
+++ controller/comments.js
@@ -13,3 +13,50 @@ exports.createComments = asyncHandler(async (req, res, next) => {
     data: comments,
   });
 });
+
+// exports.getComments = asyncHandler(async (req, res, next) => {
+//   const comments = await req.db.comments.find(req.body);
+  
+//   res.status(200).json({
+//     success: true,
+//     data: comments,
+//   });
+// });
+
+// /api/comments/:id
+exports.updateComments = asyncHandler(async (req, res, next) => {
+  let comment = await req.db.comments.findByPk(req.params.id);
+  if(!comment) {
+    throw new MyError(`${req.params.id} id-тай коммент олдсонгүй. `, 400);
+  }
+  comment = await comment.update(req.body);
+  res.status(200).json({
+    success: true,
+    data: comment,
+  });
+});
+
+
+exports.getComment = asyncHandler(async (req, res, next) => {
+  const comment = await req.db.comments.findByPk(req.params.id);
+  if(!comment) {
+    throw new MyError(`${req.params.id} id-тай коммент олдсонгүй. `, 400);
+  }
+  res.status(200).json({
+    success: true,
+    data: comment,
+  });
+});
+
+exports.deleteComments = asyncHandler(async (req, res, next) => {
+  let comment = await req.db.comments.findByPk(req.params.id);
+  if(!comment) {
+    throw new MyError(`${req.params.id} id-тай коммент олдсонгүй. `, 400);
+  }
+
+  comment = await comment.destroy();
+  res.status(200).json({
+    success: true,
+    data: comment,
+  });
+});
--- routes/comments.js
+++ routes/comments.js
@@ -3,18 +3,22 @@ const { protect, authorize } = require("../middleware/protect");
 
 const router = express.Router();
 
-const { createComments } = require("../controller/comments");
+const {
+  createComments,
+  updateComments,
+  getComment,
+  deleteComments,
+} = require("../controller/comments");
 
 // api/v1/comments
 router
   .route("/")
   .post(protect, authorize("admin", "operator", "user"), createComments);
 
-// 2. Second method:
-// router
-//   .route("/:id")
-//   .get(getCategory)
-//   .put(protect, authorize("admin", "operator"),  updateCategory)
-//   .delete(protect, authorize("admin"), deleteCategory);
+router
+  .route("/:id")
+  .get(getComment)
+  .put(protect, authorize("admin", "operator", "user"), updateComments)
+  .delete(protect, authorize("admin", "operator", "user"), deleteComments);
 
 module.exports = router;
